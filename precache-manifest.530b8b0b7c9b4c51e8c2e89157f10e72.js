0afe55838fbb88ef8fc005ae54510fe3cf95ffc7
Himanshu Kansal <himanshu@Himanshus-MacBook-Pro.local>
2020-02-16 19:45:37 +0530

    added stambha webapp

---
+++ precache-manifest.530b8b0b7c9b4c51e8c2e89157f10e72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924f76c6dc03c35ad4b21c54411f9bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6543e5c58e84091c8f8f",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "e1e7e3dc5394746cbe1b",
+    "url": "/static/css/main.679a6bcd.chunk.css"
+  },
+  {
+    "revision": "6543e5c58e84091c8f8f",
+    "url": "/static/js/2.3833f357.chunk.js"
+  },
+  {
+    "revision": "4cd46fb3e8a2aa865b2d1d607cd463b8",
+    "url": "/static/js/2.3833f357.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1e7e3dc5394746cbe1b",
+    "url": "/static/js/main.8518f7b0.chunk.js"
+  },
+  {
+    "revision": "58102e04734ff298d4f2",
+    "url": "/static/js/runtime-main.bbc6dfaf.js"
+  }
+]);
